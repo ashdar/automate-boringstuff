d76f8fbd9e686def85a9235fcb1eee8300d79671
dstrait <darin.strait@ashdar-partners.com>
2020-05-14 20:37:59 -0400

    First commit, this doesn't actually work.

---
+++ Chapter-007/date_detection.py
@@ -0,0 +1,65 @@
+#Date Detection
+#Write a regular expression that can detect dates in the DD/MM/YYYY format. 
+# Assume that the days range from 01 to 31, 
+# the months range from 01 to 12, and 
+# the years range from 1000 to 2999. 
+# Note that if the day or month is a single digit, it’ll have a leading zero.
+
+# The regular expression doesn’t have to detect correct days for each month 
+# or for leap years; it will accept nonexistent dates like 31/02/2020 or 31/04/2021. 
+# Then store these strings into variables named month, day, and year, and 
+# write additional code that can detect if it is a valid date. 
+# April, June, September, and November have 30 days, 
+# February has 28 days, and the rest of the months have 31 days. 
+# February has 29 days in leap years. Leap years are every year evenly divisible by 4, 
+# except for years evenly divisible by 100, unless the year is also evenly divisible by 400. 
+# Note how this calculation makes it impossible to make a reasonably sized regular 
+# expression that can detect a valid date.
+
+import re
+
+
+def ParseDate(StringToParse):
+    """Detect dates formatted DD/MM/YYYY. This does not verify that they are actually dates.
+    Returns [string] day, [string] month, [string] year """
+    dd='dd'
+    mm='mm'
+    yyyy='yyyy'
+
+
+    # FIXME: This pattern is not correct. 
+    # I'm testing stuff in a Python shell and on regex101.com to find something that will work.
+    # @dstrait, 2020/05/14
+    # regex = re.compile(r'([\d]{1,2})\\([\d]{1,2})\\([\d]{4})')
+    regex = re.compile(r'([\d]{1,2})')
+    mo = regex.search(StringToParse)
+
+    print(mo.group(1))
+    dd = mo.group(1)
+
+    # FIXME: will this work, once Iv'e got the RE pattern worked out?
+    # dd,mm,yyyy = mo.groups()
+
+    return(dd, mm, yyyy)
+### Main #########
+
+
+testCasesForBadDates = [
+    '31/02/2020','31/04/2021'
+    ]
+
+testCasesForGoodDates = [
+    '01/01/2020','29/02/2020'
+    ]
+
+testCasesForGoodLeapYearDates = [
+    '29/02/2020'
+    ]
+testCasesForGoodLeapYearDates = [
+    '29/02/2020'
+    ]
+
+
+for tc in testCasesForBadDates:
+    day, month, year = ParseDate(tc)
+    print (month, day, year)
