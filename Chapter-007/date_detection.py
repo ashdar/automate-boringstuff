a6ed627c893a7bdbab37c3c21e3c9abc5f1cfc6f
dstrait <darin.strait@ashdar-partners.com>
2020-05-18 14:36:36 -0400

    Final polishing. This is feature-complete, AFAIK.

--- Chapter-007/date_detection.py
+++ Chapter-007/date_detection.py
@@ -70,7 +70,6 @@ def IsValidLeapYear(yyyy):
     """Takes a string with a four-digit year and checks to see if it is a leap year
     Returns boolean"""
 
-    IsLeap = False
     # Leap years are every year evenly divisible by 4,
     # except for years evenly divisible by 100,
     # unless the year is also evenly divisible by 400.
@@ -79,13 +78,18 @@ def IsValidLeapYear(yyyy):
     # The years 1600, 2000 and 2400 are leap years,
     # while 1700, 1800, 1900, 2100, 2200 and 2300 are not leap years.
 
+    # Most years are not leap years
+    IsLeap = False
+    
+    # every 4 years is a leap year
     if (int(yyyy) % 4 == 0):
-        #     if (int(yyyy) % 400 == 0):
         IsLeap = True
 
+        # the last day of the century is not a leap year
         if (int(yyyy) % 100 == 0):
             IsLeap = False
 
+        # unless it's every fourth century
         if (int(yyyy) % 400 == 0):
             IsLeap = True
 
@@ -93,27 +97,31 @@ def IsValidLeapYear(yyyy):
 
 
 def IsValidDate(dd, mm, yyyy):
-
+    """Finds if a dd, mm, yyyy comprise a valid date
+    Returns boolean"""
+    
     IsValid = False
 
     # April, June, September, and November have 30 days,
-    #
     # February has 28 days (plus leap year issues),
-    #
     # and the rest of the months have 31 days.
+
+    # there is a case for each of the twelve months
+    # If we are passed a ficticious thirteenth month, IsValid 
+    # will remain false and that's what gets handed back to the caller
     if (mm in ['04', '06', '09', '11']):
-        if (int(dd) <= 30):
+        if (int(dd) >= 1 and int(dd) <= 30):
             IsValid = True
     elif (mm in ['02']):
         # leap years get 29 days in '02'
         if (IsValidLeapYear(yyyy)):
-            if (int(dd) <= 29):
+            if (int(dd) >= 1 and int(dd) <= 29):
                 IsValid = True
         else:
-            if (int(dd) <= 28):
+            if (int(dd) >= 1 and int(dd) <= 28):
                 IsValid = True
-    else:
-        if (int(dd) <= 31):
+    elif (mm in ['01','03','05','07','08','10','12']):
+        if (int(dd) >= 1 and int(dd) <= 31):
             IsValid = True
 
     return(IsValid)
@@ -127,7 +135,7 @@ class TestStringMethods(unittest.TestCase):
 # FIXME: Split tests for ParseDate() and IsValidDate()
 # Testing IsVAlidDate() is one thing. 
 # Testing ParseDate is a different thing, even if you rely on IsValidDate() to check that you hav a good date.
-    def test_BadDates(self):
+    def test_ParseDate_Bad(self):
         testCases = [
             '31/02/2020', '31/04/2021'
         ]
@@ -138,7 +146,7 @@ class TestStringMethods(unittest.TestCase):
                 Is = IsValidDate(day, month, year)
                 self.assertFalse(Is, "'{0}' is not a valid date".format(i))
 
-    def test_BadLeapYears(self):
+    def test_LeapYear_Bad(self):
         testCases = [
             # in the far past
             1700, 1800, 1900
@@ -154,7 +162,7 @@ class TestStringMethods(unittest.TestCase):
                 self.assertFalse(
                     Is, "'{0}' is not a valid leap year".format(i))
 
-    def test_GoodDates(self):
+    def test_ParseDate_Good(self):
         testCases = [
             '01/01/2020'            # This is a valid leap day
             , '29/02/2020'
@@ -166,7 +174,7 @@ class TestStringMethods(unittest.TestCase):
                 Is = IsValidDate(day, month, year)
                 self.assertTrue(Is, "'{0}' is not a valid date".format(i))
 
-    def test_GoodLeapYears(self):
+    def test_LeapYear_Good(self):
         testCases = [
             # in the (distant?) paste
             1600, 2000      # in the past
@@ -180,6 +188,37 @@ class TestStringMethods(unittest.TestCase):
                 self.assertTrue(Is, "'{0}' is a valid leap year".format(i))
     
 
+    def test_ValidDate_Bad(self):
+        testCases = [
+            # this has a bad month
+            {'day' : 1, 'month' : 13, 'year' : 2020}            
+            # this has a bad month
+            ,{'day' : 1, 'month' : 0, 'year' : 2020}            
+            # this has a bad day
+            ,{'day' : 0, 'month' : 1, 'year' : 2020}            
+            # this also has a bad day
+            ,{'day' : 50, 'month' : 1, 'year' : 2020}            
+            # This is NOT a valid leap day
+            ,{'day' : 29, 'month' : 2, 'year' : 2021}            
+            ]
+
+        for i in testCases:
+            with self.subTest(i=i):
+                Is = IsValidDate(i['day'], i['month'], i['year'])
+                self.assertFalse(Is, "'{0}/{1}/{2}' is not a valid date".format(i['day'], i['month'], i['year']))
+
+    def test_ValidDate_Good(self):
+        # !!!IsValidDate takes three strings, it doesn't work property against integer values!!!
+        testCases = [
+            # This is a valid leap day
+            {'day' : '01', 'month' : '01', 'year' : '2020'}            
+            ,{'day' : '29', 'month' : '02', 'year' : '2020'}            
+            ]
+
+        for i in testCases:
+            with self.subTest(i=i):
+                Is = IsValidDate(i['day'], i['month'], i['year'])
+                self.assertTrue(Is, "'{0}/{1}/{2}' is a valid date".format(i['day'], i['month'], i['year']))
 ### Main #########
 
 # To run this, with tests, from a command line in a pwsh shell:
