5d58f0766272c9a39cc84007857a01e399fdf943
dstrait <darin.strait@ashdar-partners.com>
2020-05-19 17:18:00 -0400

    Add a file

---
+++ Chapter-008/idiot.py
@@ -0,0 +1,12 @@
+# idiot.py
+# 
+# this seems uncessarily hostile.
+
+import pyinputplus as pyip
+while (True):
+    prompt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(prompt)
+    if response == 'no':
+        break
+
+print('Thank you. Have a nice day.') 
