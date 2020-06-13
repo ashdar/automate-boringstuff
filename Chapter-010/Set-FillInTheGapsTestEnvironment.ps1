f07d03ce11866fb41a9efba021f1b93448d8e7b6
dstrait <darin.strait@ashdar-partners.com>
2020-06-12 20:09:31 -0400

    Add  script to bulid test env for next project

---
+++ Chapter-010/Set-FillInTheGapsTestEnvironment.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+Creates the test files for the FillIntheGaps project
+
+.NOTES
+Sometimes, PowerShell is just easier...
+#>
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    [switch] $Clean,
+    [string] $Folder = '.\FillInTheGapsTestFiles'
+)
+
+$Prefix = 'spam'
+$FilenameTemplate = "$Prefix-0{0}.txt"
+
+if ($Clean) {
+    if (Test-Path $Folder) {
+        Write-Verbose -Message "Cleaning $folder "
+        Remove-Item $folder -Recurse -whatif
+    }
+    else {
+        Write-Verbose -Message "Found $folder was removed"
+    }
+}
+else {
+    if (Test-Path $Folder) {
+        Write-Verbose -Message "Found $folder in place"
+    }
+    else {
+        $null = mkdir $folder
+        Write-Verbose -Message "Created $folder "
+    }
+
+    1..10 | ForEach-Object { Get-Random -Minimum 1 -maximum 9 } | 
+    ForEach-Object { Set-Content -value 'foo' -path (Join-Path -Path $Folder -ChildPath ($FilenameTemplate -f $_ )) }
+    
+}
+
